cdca94b697b02018fa3ae5957012d41f13bbcc8b
Jeremy New <93681678+jayynew94@users.noreply.github.com>
2022-06-02 17:13:15 -0500

    done

--- promises.js
+++ promises.js
@@ -51,25 +51,57 @@ new Promise((resolve)=>{
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
-// Your code here
+ function wait(ms) {
+     return new Promise(resolve => setTimeout(resolve, ms))
+ }
 
+ async function justWaiting() {
+     await wait(5000)
+     console.log("we done!")
+ }
 
-
+ justWaiting()
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
+const tryRandomPromise = (random) => new Promise((resolve, reject) => {
+    if(random > 0.5) {
+        resolve("success!!!");
+    } else {
+        reject("random error");
+    }
+});
 
-// Your code here
+for (let i = 1; i < 10; i++) {
+    const random = Math.random();
+    wait(2000 + random * 1000)
+        .then(()=> tryRandomPromise(random))
+        .then(result => console.log("random try #", i , result))
+        .catch(error => console.error("random try #", i, error))
+    
+}
 
 
 
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
-// Your code here
+const tryTryAgain = async(i) => {
+    const random = Math.random();
+    await wait(3000 + random * 1000);
 
+    try {
+        const result = await tryRandomPromise(random)
+        console.log("random again #", i, result)
+    } catch (error) {
+        console.error("random again #", i, error);
+    }
+};
+
+for(let i = 1; i < 10; i++){
+    tryTryAgain(i)
+}
 
 
 /* ============================== Phase 8 ============================== */
 /* -------------------- Promises are asynchronous! --------------------- */
-
-// Your code here
+console.log("end of program");
